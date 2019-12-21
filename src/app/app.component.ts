b002b65432e0c92d286f8aae2e74e3c7f90e2c44
rkeng <rkeng@ucsd.edu>
2019-12-21 17:37:55 +0800

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from './item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ls-todo-list';
+
+  items = [
+    new Item(false, 'Hit the gym'),
+    new Item(true, 'Pay bills'),
+    new Item(false, 'Meet George'),
+    new Item(false, 'Buy eggs'),
+    new Item(false, 'Read a book'),
+    new Item(false, 'Organize office'),
+  ];
+
+  // add a new list item
+  addItem(name: string) {
+    this.items.push(new Item(false, name));
+  };
+
+  // toggle check on a list item
+  checkItem(item: Item) {
+    item.checked = !item.checked;
+  }
+
+  // remove a list item
+  removeItem(item: Item) {
+    let index = this.items.indexOf(item);
+    this.items.splice(index, 1);
+  }
 }
---
+++ src/app/item.ts
@@ -0,0 +1,5 @@
+export class Item {
+   constructor(
+      public checked: boolean,
+      public name: string) { }
+}
